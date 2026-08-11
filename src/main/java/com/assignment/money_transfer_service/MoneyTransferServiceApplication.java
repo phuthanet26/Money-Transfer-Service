6680237b418268461c2acc4dd17257cd3883b22d
Phuthanet Subsinsajjawong <phuthanet.sub@gmail.com>
2026-08-11 13:55:09 +0700

    inital project

---
+++ src/main/java/com/assignment/money_transfer_service/MoneyTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.money_transfer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTransferServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/money_transfer_service/MoneyTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.money_transfer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
